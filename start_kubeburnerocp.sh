48302a99f3efaeb4137297d85cc682537893ddab
sferlin <sferlin@redhat.com>
2025-05-09 13:07:22 +0200

    Afew options to call kube-burner-ocp

--- start_kubeburnerocp.sh
+++ start_kubeburnerocp.sh
@@ -3,4 +3,11 @@
 export ES_SERVER='https://admin:nKNQ9=vw_bwaSy1@search-perfscale-pro-wxrjvmobqs7gsyi3xvxkqmn7am.us-west-2.es.amazonaws.com:443'
 export ES_IDX='ripsaw-kube-burner'
 
-/root/sferlin/kube-burner-node-density-churn/bin/amd64/kube-burner-ocp node-density-cni --gc-metrics=true --gc=true --es-server=$ES_SERVER --es-index=$ES_IDX --churn --churn-cycles $1 --churn-percent $2 --pods-per-node $3 --worker-nodes 2 --local-indexing
+#old build, with churn for node-density-cni, but wrong churnStart/churnEnd timestamps
+#/root/sferlin/kube-burner-node-density-churn/bin/amd64/kube-burner-ocp node-density-cni --gc-metrics=true --gc=true --es-server=$ES_SERVER --es-index=$ES_IDX --churn --churn-cycles $1 --churn-percent $2 --pods-per-node $3 --worker-nodes 2 --local-indexing
+
+#correct churnStart/churnEnd correct timestamps, without --worker-nodes hack
+#/root/sferlin/kube-burner-ocp/bin/amd64/kube-burner-ocp node-density-cni --gc-metrics=true --gc=true --es-server=$ES_SERVER --es-index=$ES_IDX --churn --churn-cycles $1 --churn-percent $2 --pods-per-node $3 --local-indexing
+
+#rds
+#kube-burner-ocp rds-core --gc-metrics=true --dpdk-cores=2 --perf-profile=customcnf --gc=false --service-latency=true --churn=false --iterations 99 --es-server=https://admin:nKNQ9=vw_bwaSy1@search-perfscale-pro-wxrjvmobqs7gsyi3xvxkqmn7am.us-west-2.es.amazonaws.com:443 --es-index=ripsaw-kube-burner
